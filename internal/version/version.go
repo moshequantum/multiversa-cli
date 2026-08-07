dee9033fa5b9d56c8806476becb38b7064dfab8f
Moshe Quantum <moshequantum@gmail.com>
2026-08-06 21:33:57 -0400

    chore: abrir ciclo v0.9.3-dev

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	// Untagged builds must never impersonate an official release. GoReleaser
 	// replaces all three values for signed/tagged artifacts.
-	Version = "v0.9.2-dev"
+	Version = "v0.9.3-dev"
 	Commit  = "none"
 	Date    = "unknown"
 )
